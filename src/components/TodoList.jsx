6a23b0aa40d13be037558bc18975e1aeea05851e
shunta aoki <kaweco17@gmail.com>
2026-03-29 00:00:45 +0900

    add some codes

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const TodoList = () => {
+  return (
+    <div className='todoList'>
+        <div className='todos'>
+            <div className='todo'>
+                <div className="todoText">
+                    <span></span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
