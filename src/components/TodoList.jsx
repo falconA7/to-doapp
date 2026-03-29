d817c123b5cf6a871aa7025bf007de7d02ed72f4
shunta aoki <kaweco17@gmail.com>
2026-03-29 12:31:39 +0900

    add function of submit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,7 +6,15 @@ export const TodoList = () => {
         <div className='todos'>
             <div className='todo'>
                 <div className="todoText">
-                    <span></span>
+                    <span>プログラミング</span>
+                </div>
+                <div className='icons'>
+                    <button>
+                        <i class="fa-solid fa-check"></i>
+                    </button>
+                    <button>
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
                 </div>
             </div>
         </div>
